a3917638a767656ceb5faa09556d66b230929b19
Dod <dod256@gmail.com>
2015-11-20 15:18:31 +0300

    Some new pages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.List;
+
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hihi");
+    public static void main(String[] args){
+
     }
 }
